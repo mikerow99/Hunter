b2f1f8f36fd7f3b9d6db4030c27f87155ec536a9
Bartek Wiorkiewicz <kaktus283@gmail.com>
2012-04-22 11:46:41 +0200

    Update to 0.2.0 with fix from 0.1.x

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var a = 0;
 var b = 0;
+var version = '0.2.0';
 function addMonster ( value ) {
   for( var i = 1; i <= value; i++ ) {
     var monster = document.createElement('div');
@@ -7,7 +8,7 @@ function addMonster ( value ) {
     monster.setAttribute('onmouseover', 'remove('+b+++')');
     monster.setAttribute('class', 'monster');
     monster.style.left = Math.floor( ( Math.random( ) * 95 ) + 1) +"%";
-    monster.style.top = 3 + Math.floor( ( Math.random( ) * 5 ) + 1 ) + "%";
+    monster.style.top = 5 + Math.floor( ( Math.random( ) * 5 ) + 1 ) + "%";
     document.body.appendChild( monster );
   }
 }
